128d97badd505b069cf891f6e3753a23bd6163d6
Daniel Kerr <daniel.kerr@ihg.com>
2014-01-30 16:44:05 -0500

    Added ConnectionManagerInterface

---
+++ connectionTest/src/main/java/com/ihg/ConnectionManagerInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ihg;
+
+/**
+ *
+ * @author KerrDa
+ */
+public interface ConnectionManagerInterface {
+    public Connection getConnection();
+}
