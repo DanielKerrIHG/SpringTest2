e6acfe95a1a6a92290f73c2ed4e1f2709a325a79
Daniel Kerr <daniel.kerr@ihg.com>
2014-01-30 16:49:49 -0500

    Added ConnectionManagerInterface

---
+++ connectionTest/src/main/java/com/ihg/Connection.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ihg;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author KerrDa
+ */
+public class Connection implements ConnectionInterface{
+    public void send(String message) throws IOException{
+        System.out.println("HELLO " + message);
+    }
+    
+    public String receive() throws IOException{
+        return "HELLO";
+    }
+}
